359fe9e64713e33898e617088902d918db483229
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2026-09-03 17:28:43 +0200

    fix: fix broken unit tests

--- packages/commercelayer/src/components/Dialog.spec.tsx
+++ packages/commercelayer/src/components/Dialog.spec.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Dialog from './Dialog';
 import { render } from '@testing-library/react';
 import { mockCma, mockSdk } from '../../test/mocks';
+import { vi } from 'vitest';
+
+vi.mock('@commercelayer/sdk', () => ({
+  default: () => ({
+    skus: {
+      list: vi.fn().mockResolvedValue(
+        Object.assign([], { meta: { recordCount: 0, pageCount: 1 } })
+      ),
+    },
+  }),
+}));
 
 describe('Dialog component', () => {
-  it('Component text exists', () => {
-    const { getByText } = render(<Dialog cma={mockCma} sdk={mockSdk} />);
+  it('Component text exists', async () => {
+    const { findByPlaceholderText } = render(<Dialog cma={mockCma} sdk={mockSdk} />);
 
-    expect(getByText('Hello Dialog Component')).toBeInTheDocument();
+    expect(await findByPlaceholderText('Search for an option...')).toBeInTheDocument();
   });
 });
